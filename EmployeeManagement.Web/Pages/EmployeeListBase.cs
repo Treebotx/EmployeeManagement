bb817ca304fb3ac7c085b91059327ec7812a2ab4
Treebot <treebotx@gmail.com>
2021-04-22 14:28:22 +0100

    Add EmployeeListBase class, with hardcoded Employees.

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "johnhastings@test.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "The Original Department" },
+                PhotoPath = "images/john.png"
+            };
+
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "samgalloway@test.com",
+                DateOfBirth = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 2, DepartmentName = "Department 2" },
+                PhotoPath = "images/sam.jpg"
+            };
+
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "marysmith@test.com",
+                DateOfBirth = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "The Original Department" },
+                PhotoPath = "images/mary.png"
+            };
+
+            Employee e4 = new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "saralongway@test.com",
+                DateOfBirth = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 3, DepartmentName = "Thirdways" },
+                PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
