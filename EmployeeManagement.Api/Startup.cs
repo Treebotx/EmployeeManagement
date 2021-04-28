ba309573725f9dd86675c02a9c48ec91e934b0ac
Treebot <treebotx@gmail.com>
2021-04-28 17:37:53 +0100

    Add Employee and Department repositories to services.

--- EmployeeManagement.Api/Startup.cs
+++ EmployeeManagement.Api/Startup.cs
@@ -32,6 +32,9 @@ namespace EmployeeManagement.Api
                 options.UseSqlServer(Configuration.GetConnectionString("DBConnection"))
             );
 
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
