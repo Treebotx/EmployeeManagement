6d26475e064da2b1cf18da1eab99bdfd830df573
Treebot <treebotx@gmail.com>
2021-04-28 17:34:16 +0100

    Add DepartmentRepository.

---
+++ EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Department> GetDepartment(int departmentId)
+        {
+            return await _appDbContext.Departments
+                .FirstOrDefaultAsync(d => d.DepartmentId == departmentId);
+        }
+
+        public async Task<IEnumerable<Department>> GetDepartments()
+        {
+            return await _appDbContext.Departments.ToListAsync();
+        }
+    }
+}
