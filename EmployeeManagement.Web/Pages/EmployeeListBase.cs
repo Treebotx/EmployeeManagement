4364228a56ea0ee680dd685244a6230397cb66e6
Treebot <treebotx@gmail.com>
2021-06-15 10:39:10 +0100

    Use HttpClient from blazor app to call the api to get the employees.

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -1,73 +1,23 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeService _employeeService { get; set; }
+
         public IEnumerable<Employee> Employees { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(LoadEmployees);
-        }
-
-        private void LoadEmployees()
-        {
-            System.Threading.Thread.Sleep(3000);
-
-            Employee e1 = new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "John",
-                LastName = "Hastings",
-                Email = "johnhastings@test.com",
-                DateOfBirth = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/john.png"
-            };
-
-            Employee e2 = new Employee
-            {
-                EmployeeId = 2,
-                FirstName = "Sam",
-                LastName = "Galloway",
-                Email = "samgalloway@test.com",
-                DateOfBirth = new DateTime(1981, 12, 22),
-                Gender = Gender.Male,
-                DepartmentId = 2,
-                PhotoPath = "images/sam.jpg"
-            };
-
-            Employee e3 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Mary",
-                LastName = "Smith",
-                Email = "marysmith@test.com",
-                DateOfBirth = new DateTime(1979, 11, 11),
-                Gender = Gender.Female,
-                DepartmentId = 1,
-                PhotoPath = "images/mary.png"
-            };
-
-            Employee e4 = new Employee
-            {
-                EmployeeId = 4,
-                FirstName = "Sara",
-                LastName = "Longway",
-                Email = "saralongway@test.com",
-                DateOfBirth = new DateTime(1982, 9, 23),
-                Gender = Gender.Female,
-                DepartmentId = 3,
-                PhotoPath = "images/sara.png"
-            };
-
-            Employees = new List<Employee> { e1, e2, e3, e4 };
+            Employees = (await _employeeService.GetEmployees()).ToList() ;
         }
     }
 }
