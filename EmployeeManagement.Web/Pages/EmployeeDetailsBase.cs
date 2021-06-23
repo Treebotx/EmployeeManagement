603f3d87046043261606f8324fd083d7613fa5fa
Treebot <treebotx@gmail.com>
2021-06-23 09:50:29 +0100

    Add event to display mouse coordinates when hovering over the user image.

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web.Pages
@@ -15,10 +16,22 @@ namespace EmployeeManagement.Web.Pages
         [Parameter]
         public string Id { get; set; }
 
+        protected string Coordinates { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             Id = Id ?? "1";
             Employee = await _employeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected void EmployeeImg_Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        }
+
+        protected void EmployeeImg_Mouse_Out(MouseEventArgs e)
+        {
+            Coordinates = "";
         }
     }
 }
