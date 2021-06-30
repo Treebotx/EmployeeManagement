6882079f2c0470b543846498584396cfbdea89fe
Treebot <treebotx@gmail.com>
2021-06-30 09:43:06 +0100

    Add a page to edit employees.

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService _employeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await _employeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
