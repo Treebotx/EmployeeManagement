4f606af9edacbe95cf486415e4c4b951db0df00f
Treebot <treebotx@gmail.com>
2021-07-28 10:15:40 +0100

    Add a ConfirmEmail field to the EditEmployee page. Add a submit button to the EditEmployee page.

---
+++ EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Models.CustomValidators;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class EditEmployeeModel
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "test.com", ErrorMessage = "Domain must be test.com")]
+        public string Email { get; set; }
+        [CompareProperty(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
--- EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
@@ -15,7 +16,8 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IDepartmentService _departmentService { get; set; }
 
-        public Employee Employee { get; set; } = new Employee();
+        private Employee Employee { get; set; } = new Employee();
+        public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
         public List<Department> Departments { get; set; } = new List<Department>();
 
         [Parameter]
@@ -25,6 +27,22 @@ namespace EmployeeManagement.Web.Pages
         {
             Employee = await _employeeService.GetEmployee(int.Parse(Id));
             Departments = (await _departmentService.GetDepartments()).ToList();
+
+            EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            EditEmployeeModel.FirstName = Employee.FirstName;
+            EditEmployeeModel.LastName = Employee.LastName;
+            EditEmployeeModel.Email = Employee.Email;
+            EditEmployeeModel.ConfirmEmail = Employee.Email;
+            EditEmployeeModel.DateOfBirth = Employee.DateOfBirth;
+            EditEmployeeModel.Gender = Employee.Gender;
+            EditEmployeeModel.PhotoPath = Employee.PhotoPath;
+            EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            EditEmployeeModel.Department = Employee.Department;
+        }
+
+        protected void HandleValidSubmit()
+        {
+
         }
     }
 }
